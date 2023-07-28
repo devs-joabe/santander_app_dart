1d2ed471807f0ed2586843955e96af5f732341c4
devs-joabe <joabefnascimento1@outlook.com>
2023-07-28 02:09:02 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/joabenascimento/app/santander_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joabenascimento.app.santander_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/app_images.dart
@@ -0,0 +1,11 @@
+class AppImages {
+  static String Logo = 'lib/imgs/logo.svg';
+  static String notification = 'lib/imgs/notification.svg';
+  static String up = 'lib/imgs/up.svg';
+  static String down = 'lib/imgs/down.svg';
+  static String moneyCircle = 'lib/imgs/money_circle.svg';
+  static String handMoney = 'lib/imgs/hand_money.svg';
+  static String screwDriver = 'lib/imgs/screwdriver.svg';
+  static String circleOn = 'lib/imgs/circle_on.svg';
+  static String circleOff = 'lib/imgs/circle_off.svg';
+}
